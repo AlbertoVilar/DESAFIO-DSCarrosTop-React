a8403456f58a2b2f19fd283c3ae73cd3ea1ddd05
AlbertoVilar <albertovilar1@gmail.com>
2024-03-12 17:09:41 -0300

    First commit: Header.

---
+++ src/compnents/Header/index.tsx
@@ -0,0 +1,13 @@
+import './styles.css'
+
+export default function Header() {
+
+    return(
+        <header>
+            <div className="dsc-interface-model dsc-nav-logo">
+                <h1>DSCarrosTop</h1>
+            </div>
+        </header>
+    )
+
+}
---
+++ src/routes/Catalog/index.tsx
@@ -0,0 +1,10 @@
+import Header from '../../compnents/Header'
+import './styles.css'
+
+export default function Catalog() {
+
+    return(
+        <Header />
+    )
+
+}
